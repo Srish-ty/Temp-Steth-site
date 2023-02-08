ee7e3ec52ccff6c4d29a79f1dfd17eaa7896ead3
Srishty Mangutte <srishtymangutte@gmail.com>
2023-02-08 23:28:35 +0530

    feat: made responsive

---
+++ js/myScript.js
@@ -0,0 +1,15 @@
+
+
+let btn2 =document.getElementById('menuico');
+
+//btn2.innerHTML="This is to be clicked";
+
+btn2.addEventListener("click",()=>{
+    window.alert(101);
+    let nav2= document.getElementById('navi');
+    nav2.style.display= block;
+})
+
+function showmenu(){
+    window.alert(101);
+}
