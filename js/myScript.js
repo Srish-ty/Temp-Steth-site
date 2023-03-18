48a8c2dff18eef7049e77b1f8e2a795d0cd33434
Srishty Mangutte <srishtymangutte@gmail.com>
2023-03-19 04:28:22 +0530

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -5,6 +5,7 @@ let btn2 =document.getElementById('menuico');
 //btn2.innerHTML="This is to be clicked";
 
 btn2.addEventListener("click",()=>{
+    let btn2 =document.getElementById('menuico');
     window.alert(101);
     let nav2= document.getElementById('navi');
     nav2.style.display= block;
