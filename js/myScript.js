dfe7218e8cc24a1f5beda7429a1a00d70704715e
Srishty Mangutte <srishtymangutte@gmail.com>
2023-03-19 05:23:00 +0530

    feat: added hamburger and menu

--- js/myScript.js
+++ js/myScript.js
@@ -12,5 +12,19 @@ btn2.addEventListener("click",()=>{
 })
 
 function showmenu(){
-    window.alert(101);
+    let btn2 =document.getElementById('menuico');
+    //window.alert(101);
+    let nav2= document.getElementById('navi');
+    let ni = document.getElementsByClassName('n-i');
+    for(item of ni){
+        //item.innerHTML="hey hye";
+        if(item.style.display=='inline' || item.style.display=='block'){
+            item.style.display='none';
+        }
+        else{
+            item.style.display='block';
+        }
+    }
+    //nav2.innerHTML="Hello is hti s working";
+    
 }
